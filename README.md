d3f7b62f431b23a37b2a1f6aace9fbdba237573a
jacobhsu <jacob.hsu.tw@gmail.com>
2019-11-27 16:31:04 +0800

    https://material-ui.com/zh/components/app-bar/

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 [Material Icons](https://material-ui.com/zh/components/material-icons/)  
 `yarn add @material-ui/icons`
 
+[App Bar（应用栏）](https://material-ui.com/zh/components/app-bar/)
 [Lists（列表）](https://material-ui.com/zh/components/lists/) 列表是对文本或图像的连续、垂直的索引。  
 [Dialogs（对话框组件）](https://material-ui.com/zh/components/dialogs/) 对话框将一个任务告知给用户，它包含了一些关键信息，需要用户进行确认。
 
--- material-ui-app/src/App.js
+++ material-ui-app/src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 import SimpleList from './SimpleList';
 import SimpleDialogs from './SimpleDialogs';
+import SimpleAppBar from './SimpleAppBar';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Router >
         <Route path="/" component={SimpleList} />
         <Route path="/SimpleDialogs" component={SimpleDialogs} />
+        <Route path="/SimpleAppBar" component={SimpleAppBar} />
       </Router>
     </React.Fragment>
 
